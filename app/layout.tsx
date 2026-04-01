480fc350a5eb079806c35278e427194892c3032d
satoyume13 <reemm625@gmail.com>
2026-04-01 15:56:13 +0200

    adding Chatbot

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Chatbot from './Chatbot';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,10 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html
+      
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
+    
   );
 }
